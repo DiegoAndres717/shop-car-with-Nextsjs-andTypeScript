d660a59626caebe9108778b88587da4feb2947f5
Diego Andress Salas <diegoandress717>
2023-07-13 15:55:28 -0500

    first commit

---
+++ components/index.tsx
@@ -0,0 +1,23 @@
+import Hero from "./Hero"
+import CustomButton from "./CustomButton"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import CustomFilter from "./CustomFilter"
+import SearchBar from "./SearchBar"
+import SearchManufaturer from "./SearchManufacturer"
+import CardCar from "./CardCar"
+import CarDetails from "./CarDetails"
+import ShowMore from "./ShowMore"
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufaturer,
+    CardCar,
+    CarDetails,
+    ShowMore
+}
